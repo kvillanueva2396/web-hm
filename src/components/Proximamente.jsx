bd801278378e8a7e8a56049cc9c6a7c791425e39
jxx0911 <kevin.322.vr@gmail.com>
2021-05-06 14:21:26 -0500

    Tailwind instalado

---
+++ src/components/Proximamente.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Proximamente = () => {
+    return (
+        <div>
+            <h1 className="text-blue-500 text-4xl text-center mt-5">Proximamente...</h1>
+        </div>
+    )
+}
+
+export default Proximamente
